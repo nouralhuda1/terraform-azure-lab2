b42afce2f95890118e8236a66690eca2699ebce6
nour alhuda <noorahmeeed76@gmail.com>
2026-06-14 13:17:04 +0000

    Configure remote backend

--- providers.tf
+++ providers.tf
@@ -1,11 +1,18 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
+      version = "~> 4.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfstate6964506c"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
